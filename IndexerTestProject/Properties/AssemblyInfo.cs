073cfc67bee0efc2735c11c26b38873b061a63f3
Akio532 <sania5134@gmail.com>
2020-05-04 21:04:06 +0300

    Добавьте файлы проекта.

---
+++ IndexerTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IndexerTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("IndexerTestProject")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51e57063-2917-4275-9649-c87c3b29c8b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ task1(Csharp)/IndexerClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_cs
+{
+    public class IndexerClass
+    {
+        double[] array;
+        int firstIndex, lastIndex;
+        public IndexerClass(double[] array, int firstIndex, int length)
+        {
+            if (firstIndex < 0 || length <= 0 || array.Length <= firstIndex + length)
+                throw new ArgumentException();
+            this.array = array;
+            this.firstIndex = firstIndex;
+            this.lastIndex = firstIndex + length - 1;
+        }
+        public int Length
+        {
+            get => this.lastIndex - this.firstIndex + 1;
+        }
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Length)
+                    throw new IndexOutOfRangeException();
+                return this.array[index + firstIndex];
+            }
+            set
+            {
+                if (index < 0 || index >= this.Length)
+                    throw new IndexOutOfRangeException();
+                this.array[index + firstIndex] = value;
+            }
+        }
+
+    }
+}
